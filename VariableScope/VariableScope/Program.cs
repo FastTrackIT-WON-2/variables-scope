ac76db909a469ae3014cb10d0152de359b5157f8
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-04-12 19:46:26 +0300

    Demonstrate usage of block scope

---
+++ VariableScope/VariableScope/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace VariableScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockScope();
+        }
+
+        private static void BlockScope()
+        {
+            {
+                // here is a block scope (parent)
+                int i = 10;
+                i = i + 30;
+                Console.WriteLine(i);
+
+                {
+                    // here is a child block scope
+
+                    // Not Allowed: we cannot define another variable with the same 
+                    // name as a variable defined in the parent scope!
+                    // int i = 5;
+
+                    // we can re-use variables defined in the parent block scope
+                    i = i * 5;
+                    Console.WriteLine(i);
+                }
+            }
+
+            // there are specific c# keywords that open block scopes
+            // e.g.: if, for, etc
+            if (true)
+            {
+                // here is another block scope
+            }
+        }
+    }
+}
